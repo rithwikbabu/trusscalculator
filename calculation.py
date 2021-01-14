ab143d149a8d235b482e82e2718de863b80dd108
Rithwik Babu <54031798+rithwikbabu13@users.noreply.github.com>
2021-01-14 16:59:00 -0600

    Finished whole truss calculation
    Made it capable of solving the entire truss.

--- calculation.py
+++ calculation.py
@@ -8,12 +8,23 @@ class TrussData:
         self.bdLbs = bdLbs
 
     def WholeCalc(self):
-        cyLbs = self.bdLbs * self.adLen / (self.dcLen + self.adLen)
-        ayLbs = self.bdLbs - cyLbs
+        cyLbs = round(self.bdLbs * self.adLen / (self.dcLen + self.adLen))
+        ayLbs = round(self.bdLbs - cyLbs)
         axLbs = 0
 
         aAng = math.atan(self.bdLen / self.adLen)
         cAng = math.atan(self.bdLen / self.dcLen)
 
-        
-        
+        abLbs = round(ayLbs / math.sin(aAng) * -1)
+        adLbs = round(abLbs * math.cos(aAng) * -1)
+
+        bcLbs = round(cyLbs / math.sin(cAng) * -1)
+        dcLbs = round(bcLbs * math.cos(cAng))
+
+        aAng = round(aAng / math.pi * 180)
+        cAng = round(cAng / math.pi * 180)
+
+        bdLbs = self.bdLbs*-1
+
+        print(cyLbs, ayLbs, axLbs, aAng, cAng, abLbs, adLbs, bcLbs, dcLbs, bdLbs)
+
